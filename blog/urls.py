eab796a505f8ba6063f1a0829f141d315b6d39af
mnurda <trim.arts007@gmail.com>
2020-12-10 16:42:23 +0600

    url warning

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    path(r'^index/$', views.post_list, name='index'),
-    path(r'^confirm/$', views.home_page, name='confirm'),
+    path('', views.post_list, name='index'),
+    path('', views.home_page, name='confirm'),
 ]
     # path('', views.Homeview.as_view(), name='post_list'),
     # path('', views.Homeview.as_view(), name='save_client'),
